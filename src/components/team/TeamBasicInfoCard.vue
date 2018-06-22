6bf600eb95ad85f8581914e631d3f0d526578db1
grassclipper <grassclipper@users.noreply.github.com>
2018-06-22 16:25:33 -0500

    Added cards to team home

---
+++ src/components/team/TeamBasicInfoCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <md-card>
+        <md-card-header>
+            <div class="md-title">
+                Number and Name
+            </div>
+        </md-card-header>
+
+        <md-card-content>
+            Location
+            <p> League </p>
+            <p> Year started </p>
+        </md-card-content>
+
+    </md-card>
+</template>
+
+<script>
+export default {
+    name: "TeamBasicInfoCard"
+}
+</script>
+
+<style>
+
+</style>
