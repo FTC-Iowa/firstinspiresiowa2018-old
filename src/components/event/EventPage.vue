3dacffe9ccad4556c2e971896756dde5e766207b
jvens <jeramie.vens@gmail.com>
2018-06-22 20:38:18 -0500

    Match card start

--- src/components/event/EventPage.vue
+++ src/components/event/EventPage.vue
@@ -13,7 +13,7 @@
                     <md-tab id="tab-schedule" md-label="Schedule" to="./schedule"></md-tab>
                     <md-tab id="tab-teams" md-label="Teams" to="./teams"></md-tab>
                     <md-tab id="tab-matches" md-label="Matches" to="matches"></md-tab>
-                    <md-tab id="tab-results" md-label="Results" to="results"></md-tab>
+                    <!-- <md-tab id="tab-results" md-label="Results" to="results"></md-tab> -->
                     <md-tab id="tab-rankings" md-label="Rankings" to="rankings"></md-tab>
                     <md-tab id="tab-inspections" md-label="Inspections" to="inspections"></md-tab>
                     <md-tab id="tab-awards" md-label="Awards" to="awards"></md-tab>
---
+++ src/components/match/MatchCard.vue
@@ -0,0 +1,112 @@
+<template>
+    <md-card ><!--class="md-primary" md-theme="green-card"-->
+        <md-card-header>
+            <md-card-header-text>
+                <div class="md-title">Match {{match.name}}</div>
+                <div class="md-subhead">Red {{match.red.score.total}}-{{match.blue.score.total}}</div>
+                <div class="columns">
+                    Red Aliance
+                    <ul>
+                        <li>12345</li>
+                        <li>1155</li>
+                    </ul>
+                </div>
+                <div class="columns">
+                    Blue Aliance
+                </div>
+            </md-card-header-text>
+            <!-- <md-card-header-text>
+                some text
+            </md-card-header-text> -->
+        </md-card-header>
+
+        <md-card-expand>
+            <md-card-expand-content>
+                <md-card-content>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+                </md-card-content>
+            </md-card-expand-content>
+            <md-card-actions md-alignment="right">
+                <md-card-expand-trigger>
+                    <md-button class="md-icon-button">
+                        <md-icon>keyboard_arrow_down</md-icon>
+                    </md-button>
+                </md-card-expand-trigger>
+            </md-card-actions>
+
+        </md-card-expand>
+    </md-card>
+</template>
+
+<script>
+export default {
+    name: "MatchCard",
+    data: () => ({
+        match: {
+            name: "Q-1",
+            red: {
+                teams: [{
+                        number: 6596,
+                        surrogate: false
+                    },{
+                        number: 12835,
+                        surrogate: false
+                    }
+                ],
+                score: {
+                    autonomous: 125,
+                    autobonus: 0,
+                    teleop: 86,
+                    endgame: 20,
+                    penalties: 0,
+                    total: 231
+                }
+            },
+            blue: {
+                teams: [{
+                        number: 8813,
+                        surrogate: false
+                    },{
+                        number: 9968,
+                        surrogate: false
+                    }
+                ],
+                score: {
+                    autonomous: 140,
+                    autobonus: 0,
+                    teleop: 56,
+                    endgame: 55,
+                    penalties: 0,
+                    total: 251
+                }
+            },
+            starttime: {
+                seconds: 1528569639,
+                milliseconds: 0
+            }
+        }
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~vue-material/dist/theme/engine";
+
+@include md-register-theme("green-card", (
+    primary: md-get-palette-color(red, 500)
+));
+
+@import "~vue-material/dist/base/theme";
+@import "~vue-material/dist/components/MdCard/theme";
+
+.md-card {
+    margin: 8px;
+    vertical-align: top;
+}
+
+.columns {
+    width: 50%;
+    float: left;
+}
+
+</style>
