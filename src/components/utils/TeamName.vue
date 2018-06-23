e3611d4c86199d145cce59da2a044c88a63c6992
jvens <jeramie.vens@gmail.com>
2018-06-22 23:28:42 -0500

    Resolve a team number into a name

---
+++ src/components/utils/TeamName.vue
@@ -0,0 +1,25 @@
+<template>
+    <span>
+        {{ team.name }}
+    </span>
+</template>
+
+<script>
+import {db} from '../../main'
+export default {
+    name: "TeamName",
+    props: ["number"],
+    data: () => ({
+        team: {name: "Team Name Unknown"}
+    }),
+    firestore() {
+        return {
+            team: db.collection("teams").doc(this.number)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
